bc1292c10aab2e724a32760286635ebe0f7b676e
claudiay <yadathi@gmail.com>
2012-08-12 23:49:51 -0700

    continued writing setplants.py

--- setplants.py
+++ setplants.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python
-"""
-Returns a dictionary of plant placement on the graph.
-"""
+# Returns a dictionary of plant placement on the graph.
 from grid import generate_squares, generate_peers
 
 def parse_grid(squares, picked_plants):
@@ -10,7 +8,7 @@ def parse_grid(squares, picked_plants):
     # then assign values from the grid, if any. (add feature later)
     return dict((s, picked_plants) for s in squares)
 
-def grid_values(grid, squares):
+def prepicked_values(grid, squares):
     """Convert grid into a dict of {square:char} with "." for empty."""
     # This is needed for the added feature in parse_grid.
     # Not currently in use.
@@ -19,16 +17,37 @@ def grid_values(grid, squares):
 def assign(values, square, plant):
     """Eliminates all other values besides plant from square.
     Elminates plant from all other squares."""
-    values[square]=[plant]
+    values[square]=[plant] # assign plant to plot
     for plot in values:
-        if plot != square:
-	    print plot
-	    if values[plot].count(plant) >= 1:
-	        i = values[plot].index(plant)
-		print i
-		values[plot].pop(i)
-		print values[plot]
+        # remove plant from all other plots
+        if plot != square and values[plot].count(plant) >= 1:
+	    i = values[plot].index(plant) 
+	    values[plot].pop(i) # removed assigned plant from list
     return values
+
+def square_benefit(values, plant, square, guide, peers):
+    """Returns benefit of placing the plant in that square."""
+    score = 0
+    square_peers = peers[square]
+
+    # Check benefit/cost for assigned squares.
+    for peer in square_peers:
+        if len(values[peer]) == 1: # if assigned
+	    if values[peer][0][:-1] in guide[plant]['avoid']:
+	        score = score - 5
+	    elif values[peer][0][:-1] in guide[plant]['friend']:
+	        score = score + 3
+	    elif values[peer][0][:-1] == plant:
+	        score = score - 1
+    return score
+
+def keep_assigning(values):
+    """Checks to see if all plants are set."""
+    for plot in values:
+    	if len(values[plot]) > 1:
+	    return True
+	else:
+	    return False
 
 def main():
     peers = generate_peers(3,3)
@@ -45,9 +64,11 @@ def main():
 	     	{'friend':[], 'avoid':['corn', 'potato']}
 	    }
     values = parse_grid(squares, picked_plants)
-    print assign(values, "A1", "corn1")
-    print assign(values, "B1", "corn2")
-
+    assign(values, "A1", "strawberry1")
+    assign(values, "A3", "strawberry2")
+    assign(values, "B2", "potato1")
+    print square_benefit(values, "tomato", "A2", guide, peers)
+ 
 
 
 if __name__ == '__main__':
