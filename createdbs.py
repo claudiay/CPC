e195bd969530f391496f306cba14c5716d85c4ba
claudiay <c@curlcurl.in>
2012-08-15 08:26:49 -0700

    works with new data

--- createdbs.py
+++ createdbs.py
@@ -4,34 +4,29 @@ This program creates new databases for all tables included in model.py.
 import json, model
 from model import Plants, Location
 
-def upload_locations(file):
-    f = open(file)
+def upload_locations(file_name):
+    f = open(file_name)
     for line in f:
-        l = line.strip().replace(' " ', "").replace(' "', "").replace('"', "").split(",")
-        print l
-        location = Location(*l)
+        entry = line.strip().replace(' " ', "").replace(' "', "").replace('"', "").split(",")
+        print entry
+        location = Location(*entry)
         model.add(location)
         model.save_all()
-
-def upload_plants(plant_data_file):
-    # Grab the json string from data.txt
-    f = open(plant_data_file, 'r')
-    io = f.read()
-    plant_dict = json.loads(io)
     f.close()
-    
-    # Upload plants into the database
-    dict_type = ['veg', 'flo', 'her']
-    for _dict in dict_type:
-    	p_dict = plant_dict[_dict]
-	for key in p_dict:
-	    entry = [key, p_dict[key]["plant_type"], p_dict[key]["scientific_name"],
-	    	p_dict[key]["helps"], p_dict[key]["helped_by"], p_dict[key]["attracts"],
-		p_dict[key]["repels"], p_dict[key]["avoid"], p_dict[key]["comment"]]
-            print entry
-	    plant = Plants(*entry)
-	    model.add(plant)
-    	    model.save_all()
+
+def upload_plants(file_name):
+    # Grab the json string from data.txt
+    f = open(file_name)
+    for line in f:
+        line = line.strip()
+        info = line.split("|")
+        entry = [info[0], info[1].split(","), info[2].split(",")]
+        print entry
+        plant = Plants(*entry)
+        model.add(plant)
+        model.save_all()
+    f.close
+        
 
 def check():
     print "Are you sure you want to create all new updated databased?"
@@ -48,7 +43,7 @@ def main():
     if run == True:
         model.db.create_all()
 	upload_locations("plantdata/zips.csv")
-	upload_plants('plantdata/data.txt')
+	upload_plants('plantdata/newdata.txt')
 
 
 if __name__ == '__main__':
